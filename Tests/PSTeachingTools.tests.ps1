2d2a93d1592ed28208492665857da8b08e02c9d3
Jeff Hicks <jhicks@jdhitsolutions.com>
2020-05-16 09:26:13 -0400

    revised pester test

--- Tests/PSTeachingTools.tests.ps1
+++ Tests/PSTeachingTools.tests.ps1
@@ -1,9 +1,6 @@
-$here = Split-Path -Parent $MyInvocation.MyCommand.Path
-$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
-. $here\..\$sut
 
 Describe PSTeachingTools {
-    It "does something useful" {
+    It "Is under development" {
         $true | Should Be $true
     }
 }
